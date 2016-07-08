79e44b5862f854df173e0efe2b6ea56778cae9e3
butcallmejo <josquin.gaillard@gmail.com>
2016-07-08 13:11:02 -0700

    sql script finished??

---
+++ sql/init.sql
@@ -0,0 +1,12 @@
+-- Creating 2 users for the db: dev and prod
+CREATE USER 'airbnb_user_dev'@'%' IDENTIFIED BY 'development1';
+CREATE USER 'airbnb_user_prod'@'localhost' IDENTIFIED BY 'production1';
+
+-- Creating databases: dev and prod
+CREATE DATABASE 'airbnb_dev' CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE 'airbnb_prod' CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- Granting PRIVILEGES to users
+GRANT ALL PRIVILEGES ON airbnb_dev . * TO 'airbnb_user_dev'@'localhost';
+GRANT ALL PRIVILEGES ON airbnb_dev . * TO 'airbnb_user_dev'@'%';
+GRANT ALL PRIVILEGES ON airbnb_prod . * TO 'airbnb_user_prod'@'localhost';
