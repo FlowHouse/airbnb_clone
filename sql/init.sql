2d47f76fe013064241a4cbc1925e6e1ca2d66378
root <krisbredemeier@gmail.com>
2016-07-08 20:59:45 +0000

    blahblahblah

--- sql/init.sql
+++ sql/init.sql
@@ -3,8 +3,8 @@ CREATE USER 'airbnb_user_dev'@'%' IDENTIFIED BY 'development1';
 CREATE USER 'airbnb_user_prod'@'localhost' IDENTIFIED BY 'production1';
 
 -- Creating databases: dev and prod
-CREATE DATABASE 'airbnb_dev' CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE 'airbnb_prod' CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE airbnb_dev  CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE airbnb_prod CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- Granting PRIVILEGES to users
 GRANT ALL PRIVILEGES ON airbnb_dev . * TO 'airbnb_user_dev'@'localhost';
