f442433b70443bc2349bc4c589ef7b01c14525ad
Kris Bredemeier <34@holbertonschool.com>
2016-07-12 10:54:58 -0700

    works on task 2

--- api/config.py
+++ api/config.py
@@ -8,7 +8,19 @@ if condition:
 	DATABASE = {
 		host = # IP address for web-01
 		user = 'airbnb_user_dev'
-		database = 
+		database =
 	}
 # PROD
 else:
+	DEBUG: FALSE
+	HOST: 0.0.0.0
+	PORT: 3000
+	DATABASE: {
+		'host': # 158.69.91.82,
+		'user': 'airbnb_user_prod',
+		'database': 'airbnb_prod',
+		'port': 3006,
+		'charset': utf8,
+		'password': os.environ.get('AIRBNB_DATABASE_PWD_PROD', ' ')
+
+	}
