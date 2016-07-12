81ab5ab58e6f3b105ff9be66bb91d3a79f119a3b
butcallmejo <josquin.gaillard@gmail.com>
2016-07-12 10:55:04 -0700

    update config with if else if to set variables depending on environment

--- api/config.py
+++ api/config.py
@@ -1,14 +1,27 @@
-ENV = $AIRBNB_ENV
-
+ENV = os.environ.get('AIRBNB_ENV', '')
 # DEV
-if condition:
+if ENV == 'development':
 	DEBUG = True
 	HOST = localhost
 	PORT = 3333
 	DATABASE = {
-		host = # IP address for web-01
-		user = 'airbnb_user_dev'
-		database = 
+		'host': # IP address for web-01,
+		'user':'airbnb_user_dev',
+		'database': 'airbnb_dev',
+		'port': 3306,
+		'charset': 'utf8',
+		'pasword': os.environ.get('AIRBNB_DATABASE_PWD_DEV', '')
 	}
 # PROD
-else:
+else if ENV == 'production':
+	DEBUG = False
+	HOST = 0.0.0.0
+	PORT = 3000
+	DATABASE = {
+		'host': # IP address for web-01,
+		'user':'airbnb_user_prod',
+		'database': 'airbnb_prod',
+		'port': 3306,
+		'charset': 'utf8',
+		'pasword': os.environ.get('AIRBNB_DATABASE_PWD_PROD', '')
+	}
