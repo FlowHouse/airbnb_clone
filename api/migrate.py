2c9557da5dd96e96a498575bc32767b63b54ad4f
butcallmejo <josquin.gaillard@gmail.com>
2016-07-13 10:38:40 -0700

    created migrate + added imports

---
+++ api/migrate.py
@@ -0,0 +1,10 @@
+import peewee
+# hacky way of importing models
+from app.models.amenity import *
+from app.models.base import *
+from app.models.city import *
+from app.models.place_amenity import *
+from app.models.place_book import *
+from app.models.place import *
+from app.models.state import *
+from app.models.user import *
