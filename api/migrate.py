0155c45b489ecc87e434cc8842463e11373c21ff
butcallmejo <josquin.gaillard@gmail.com>
2016-07-13 10:42:05 -0700

    added models to migrate

--- api/migrate.py
+++ api/migrate.py
@@ -8,3 +8,14 @@ from app.models.place_book import *
 from app.models.place import *
 from app.models.state import *
 from app.models.user import *
+
+models = {
+	'amenity': Amenity,
+	'base': Base,
+	'city': City,
+	'place_amenity': Place_amenity,
+	'place_book': Place_book,
+	'place': Place,
+	'state': State,
+	'user': User
+}
