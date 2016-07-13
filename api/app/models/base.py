2f63a9f4fbebbee8406e68be45d459362f38c150
Kris Bredemeier <34@holbertonschool.com>
2016-07-13 09:47:52 -0700

    creates basic base function

--- api/app/models/base.py
+++ api/app/models/base.py
@@ -1,3 +1,14 @@
-import Peewee as pw
+import Peewee as peewee
+from datetime import datetime
+#import config
 
 database = pw.MySQLDatabase('')
+
+class BaseModel(peewee.Model):
+    id = peewee.PrimaryKeyField(unique = True)
+    update_at = peewee.DateTimeField(defalt = datetime.now().strftime('%Y/%m/%d %H:%M;%S'))
+    create_at = peewee.DateTimeField(defalt = datetime.now().strftime('%Y/%m/%d %H:%M;%S'))
+
+    def save(self, *args, *kwargs):
+        self.update_at = datetime.now().strftime('%Y/%m/%d %H:%M;%S')
+        peewee.Model.save(self)
