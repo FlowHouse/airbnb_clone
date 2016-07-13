95a093994b463d6d3415b8fe08cfe2d5156ad748
Kris Bredemeier <34@holbertonschool.com>
2016-07-13 12:42:21 -0700

    continues to add to indx.py

--- api/app/views/index.py
+++ api/app/views/index.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_json import FlaskJSON, json_response
 import peetwee
+from datetime import datetime
+
+local_tz = timezone('America/Los_Angeles')
 
 @app.route('/', methods=['GET'])
 def index():
+    return json_response(status='OK', utc_time=datetime.utcnow()strftime('%m/%d/%Y %H:%M:%S'), time =utc_to_local=(datetime.utcnow()).strftime('%m/%d/%Y %H:%M:%S'))
 
-@app.errorhandler(404)
 def before_request():
     models.database.connect()
 
@@ -13,5 +16,10 @@ def after_request():
     models.database.close()
     return response
 
-def not_found():
-    return json_response(add_status_=False, code=404, msg="response not found")
+@app.errorhandler(404)
+def not_found(e):
+    return json_response(add_status_=False, code=404, msg="not found")
+
+def utc_to_local(utc_dt):
+    local_dt = utc_dt.replace(tzinfo=utc).astimezone(local_tz)
+    return local_tz.normalize(local_dt)
