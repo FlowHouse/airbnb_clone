0b95ac0fbf957699f739d02828e765ac1c4a1a66
butcallmejo <josquin.gaillard@gmail.com>
2016-07-15 11:00:16 -0700

    added functions to index.py in tests. TODO:populate functions

--- personal_tests/tests/index.py
+++ personal_tests/tests/index.py
@@ -1,1 +1,13 @@
 # test github pull from head to forked repo
+
+# to create a test client of app
+def setUp(self):
+
+# to validate if status of the JSON resp of GET / is equal to OK
+def test_200(self):
+
+# to validate if time of the JSON resp of GET / is equal of local time
+def test_time(self):
+
+# to validate if utc time of the JSON resp of GET / is equal of utc time
+def test_time_utc(self):
