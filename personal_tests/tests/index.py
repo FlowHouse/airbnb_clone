00500d284610260dc19c8711f952eced3f9f9c9d
butcallmejo <josquin.gaillard@gmail.com>
2016-07-17 11:30:35 -0700

    small changes to the tests for index.py

--- personal_tests/tests/index.py
+++ personal_tests/tests/index.py
@@ -3,8 +3,11 @@
 # to create a test client of app
 def setUp(self):
 
-# to validate if status of the JSON resp of GET / is equal to OK
+# to validate if status of the JSON resp of GET / is equal to 200
 def test_200(self):
+
+# to validate if status of the JSON resp of GET / is equal to OK
+def test_status(self):
 
 # to validate if time of the JSON resp of GET / is equal of local time
 def test_time(self):
